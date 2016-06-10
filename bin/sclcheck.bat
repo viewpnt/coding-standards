80fd03e24cf75f62af9ffa619ef9a5256ec935ea
Christian Graf <christian.graf@cyberfox.at>
2016-06-10 12:43:59 +0200

    fix(): find the correct vendor directory
    - now the vendor directory could be found in case of use it within phing task and use it as composer package bin
    - i adept the phing task ... add the command php to the system calls, so it works also on windoof envrionments

---
+++ bin/sclcheck.bat
@@ -0,0 +1,4 @@
+@ECHO OFF
+setlocal DISABLEDELAYEDEXPANSION
+SET BIN_TARGET=%~dp0/sclcheck
+php "%BIN_TARGET%" %*
---
+++ bin/sclfix.bat
@@ -0,0 +1,4 @@
+@ECHO OFF
+setlocal DISABLEDELAYEDEXPANSION
+SET BIN_TARGET=%~dp0/sclfix
+php "%BIN_TARGET%" %*
